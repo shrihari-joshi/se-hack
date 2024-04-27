273b3f4035987be2c0c2fa3b9f5ac9a74156b9ad
shrihari-joshi <joshishrihari13@gmail.com>
2024-04-28 04:08:01 +0530

    try1

--- farmer/src/Components/Fruits/Addfruit.jsx
+++ farmer/src/Components/Fruits/Addfruit.jsx
@@ -1,32 +1,134 @@
-import React, { useState } from 'react';
+// ProductForm.js
 
-const FruitsItem = ({ fruit, addToCart }) => { 
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './Addfruit.css'; // Import CSS file for styling
+import FruitsItem from './FruitsItem';
+
+function ProductForm() {
+    // State variables for form fields
+    const [productName, setProductName] = useState('');
+    const [dateOfHarvest, setDateOfHarvest] = useState(new Date());
+    const [expiryDate, setExpiryDate] = useState(new Date());
+    const [quantity, setQuantity] = useState('');
+    const [price, setPrice] = useState('');
+    const [offer, setOffer] = useState('');
+    const [description, setDescription] = useState('');
+
+    const user = JSON.parse(localStorage.getItem('user'))
+    // Function to handle form submission
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        // Handle form submission logic here
+        try{
+            const response = await axios.post('http://localhost:3500/addproduct', {
+                farmername: user.username || user.farmername,
+                productName: productName,
+                category: "fruit",
+                dateOfHarvest: dateOfHarvest,
+                dateOfExpiry: expiryDate,
+                price: price,
+                offer: offer,
+                description: description,
+                // description: description,
+                quantity: quantity
+            })
+            console.log(response.data);
+        }
+        catch(error){
+            console.log(error.message)
+        }
+    };
+
+    const [fruits, setFruits] = useState([]);
+    const userData = localStorage.getItem('currentUser');
+    //   const user = userData ? JSON.parse(userData) : null;
+
+    const notifyError = (msg) => toast.error(msg, { position: 'top-center' });
+    const notifyWarning = (msg) => toast.warn(msg, { position: 'top-center' })
+    const notifySuccess = (msg) => toast.success(msg, { position: 'top-center' });
+
+    const fetchFruits = async () => {
+        try {
+            const response = await axios.get('http://localhost:3500/getallproducts', {
+                params: {
+                    category: 'fruit'
+                }
+            });
+            console.log(response.data);
+            setFruits(response.data);
+        } catch (error) {
+            notifyError(error.message);
+        }
+    };
+
+    useEffect(() => {
+        fetchFruits();
+    }, []);
+
 
     return (
-        <div className="fruit-container">
-            <h3 className="fruit-name">{fruit.productName}</h3>
-            <p className="fruit-price"><strong>Price:</strong> {fruit.price}</p>
-            <p>Quantity: {fruit.quantity}</p>
-            <p>Offers: {fruit.offers}</p>
-            <p className="fruit-date"><strong>Date of Harvest:</strong> {fruit.dateOfHarvest}</p>
-            {fruit.offers !== 0 && (
-                <p className="fruit-offers"><strong>Offers</strong> {fruit.offers}</p>
-            )}
-            {/* <p className="fruit-image"><strong>""Idhar Image dalna hai SHREYAA""</strong> {fruit.imageUrl}</p> */}
-            <p>Discription : {fruit.discription}</p>
-            {/* <input
-                type="number"
-                min="1"
-                value={selectedQuantity}
-                onChange={handleQuantityChange}
-                className="fruit-quantity"
-            /> */}
-            {/* <div className="btn-container">
-                <button onClick={() => addToCart(fruit, selectedQuantity)} className="fruit-btn">Add To Cart</button>
+        <div className='fruits'>
+
+            <div className="sj-product-form-container-custom">
+                <div className='fruits-Div'>
+                <h2>Add Fruits</h2>
+                <form className='Fruit-container' onSubmit={handleSubmit}>
+                    <div className="w-3/4 sj-form-group-custom ">
+                        <input type="text" id="productName" className='fruit-input' placeholder='Product Name' value={productName} onChange={(e) => setProductName(e.target.value)} />
+                        <span class="input-border"></span>
+                    </div>
+
+                    <div className="sj-form-group-custom">
+                        <input type="number " id="quantity" className='fruit-input' placeholder='Quantity' value={quantity} onChange={(e) => setQuantity(e.target.value)} />
+                        <span class="input-border"></span>
+                    </div>
+
+                <div className='sj-product-form'>
+                    <div className="sj-form-group-custom fruit-date1">
+                        <input type="date" id="dateOfHarvest" className='fruit-input' placeholder='Harvest-Date' value={dateOfHarvest.toISOString().substr(0, 10)} onChange={(e) => setDateOfHarvest(new Date(e.target.value))} />
+                        <span class="input-border"></span>
+                    </div>
+
+                    <div className="sj-form-group-custom fruit-date2">
+                    <input type="date" id="expiryDate" className='fruit-input' placeholder='Expiry-date'  value={expiryDate.toISOString().substr(0, 10)} // Convert Date to ISO date format
+                        onChange={(e) => setExpiryDate(new Date(e.target.value))} />
+                        <span class="input-border"></span>
+                    </div>
+                </div>
+                
+                    <div className="sj-form-group-custom">
+                    <input type="number" id="price" className='fruit-input' placeholder='Price'  value={price} onChange={(e) => setPrice(e.target.value)} />
+                        <span class="input-border"></span>
+                    </div>
+                    <div className="sj-form-group-custom">
+                        <textarea id="description" className='fruit-input' placeholder='Description' />
+                        <span class="input-border"></span>
+                    </div>
+                    <button type="submit" className='sj-button-custom'>Add Product</button>
+                </form>
             </div>
-            */}
+            </div>
+
+            <div className='sj-product-right'>
+                <ul className='sj-products'>
+                    {fruits.map((fruit, index) => (
+                        <li key={index}>
+                            <div>
+                                <FruitsItem
+                                    fruit={fruit}
+                                // addToCart={addToCart}
+                                />
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="sj-product-form-container-custom1"></div>
         </div>
     );
-};
+}
 
-export default FruitsItem;
+export default ProductForm;
