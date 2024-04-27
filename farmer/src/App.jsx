4f5baf55f216c07667fda3d261db97d803df7c57
shrihari-joshi <joshishrihari13@gmail.com>
2024-04-27 13:41:27 +0530

    start1

---
+++ farmer/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import FarmerDashBoard from './Components/FarmerDashBoard/FarmerDashBoard';
+function App() {
+  return (
+    <div className="App">
+        <FarmerDashBoard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ farmer/src/Components/FarmerDashBoard/FarmerDashBoard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./FarmerDashBoard.css"
+const FarmerDashBoard = () => {
+    return (
+        <div className='farmer-container'>
+            <div className='farmer-left'>
+            </div>
+            <div className='farmer-right'>
+                <div className='shadow-patch'></div>
+            </div>
+
+        </div>
+    )
+}
+
+export default FarmerDashBoard;
